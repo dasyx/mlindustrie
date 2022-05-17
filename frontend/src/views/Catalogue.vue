9ec27ac5959e6d48099618287f7f2449d6315e32
dasyx <seanpnn00@gmail.com>
2022-05-18 01:44:27 +0200

    ajout tableau + js dans catalogue.vue

--- frontend/src/views/Catalogue.vue
+++ frontend/src/views/Catalogue.vue
@@ -1,14 +1,388 @@
 <template>
-<Header />
-<table></table>
-<Footer />
+  <Header />
+
+  <table class="table is-striped is-bordered">
+    <tr>
+      <th colspan="3"></th>
+      <th colspan="2"></th>
+      <th colspan="3" class="has-text-centered is-vcentered">ANIMATION</th>
+    </tr>
+    <tr>
+      <th colspan="3"></th>
+      <th colspan="2"></th>
+      <th colspan="3" class="has-text-centered is-vcentered">2022</th>
+    </tr>
+    <tr>
+      <th></th>
+      <th colspan="4" class="has-text-centered is-vcentered">
+        Caractèristiques 2022
+      </th>
+      <th colspan="3" class="has-text-centered is-vcentered">
+        Coût en € HT/jour hors frais pour :
+      </th>
+    </tr>
+    <tr class="th th-large">
+      <th class="has-text-centered is-vcentered" id="large">
+        TYPE DE FORMATION
+      </th>
+      <th class="has-text-centered is-vcentered">Effectif maxi par groupe</th>
+      <th class="has-text-centered is-vcentered">
+        Nombre de personnes par configuration matérielle
+      </th>
+      <th class="has-text-centered is-vcentered">Coût par configuration</th>
+      <th class="has-text-centered is-vcentered">
+        Coût en €, HT/jour sans matériel et hors frais
+      </th>
+      <th class="has-text-centered is-vcentered">1 à 2 personnes</th>
+      <th class="has-text-centered is-vcentered">2 à 4 personnes</th>
+      <th class="has-text-centered is-vcentered">4 à 6 personnes</th>
+    </tr>
+    <tr>
+      <td>Ingénierie pédagogique</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>800 €</td>
+      <td>800 €</td>
+      <td>800 €</td>
+      <td>800 €</td>
+    </tr>
+    <tr>
+      <td>Ingénierie opérationnelle</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>800 €</td>
+      <td>800 €</td>
+      <td>800 €</td>
+      <td>800 €</td>
+    </tr>
+    <tr>
+      <td>Automates programmables généralités</td>
+      <td>6</td>
+      <td>2</td>
+      <td>30 €</td>
+      <td>800 €</td>
+      <td>830 €</td>
+      <td>860 €</td>
+      <td>890 €</td>
+    </tr>
+    <tr>
+      <td>Automates programmables - Le langage de programmation</td>
+      <td>55</td>
+      <td>53</td>
+      <td>42</td>
+      <td>150</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Automates programmables - Maintenance</td>
+      <td>80</td>
+      <td>55</td>
+      <td>75</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie opérationnelle</td>
+      <td>70</td>
+      <td>65</td>
+      <td>40</td>
+      <td>175</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Hina</td>
+      <td>70</td>
+      <td>52</td>
+      <td>63</td>
+      <td>185</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Johny</td>
+      <td>55</td>
+      <td>53</td>
+      <td>42</td>
+      <td>150</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie pédagogique</td>
+      <td>80</td>
+      <td>55</td>
+      <td>75</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie opérationnelle</td>
+      <td>70</td>
+      <td>65</td>
+      <td>40</td>
+      <td>175</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Hina</td>
+      <td>70</td>
+      <td>52</td>
+      <td>63</td>
+      <td>185</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Johny</td>
+      <td>55</td>
+      <td>53</td>
+      <td>42</td>
+      <td>150</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie pédagogique</td>
+      <td>80</td>
+      <td>55</td>
+      <td>75</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie opérationnelle</td>
+      <td>70</td>
+      <td>65</td>
+      <td>40</td>
+      <td>175</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Hina</td>
+      <td>70</td>
+      <td>52</td>
+      <td>63</td>
+      <td>185</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Johny</td>
+      <td>55</td>
+      <td>53</td>
+      <td>42</td>
+      <td>150</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie pédagogique</td>
+      <td>80</td>
+      <td>55</td>
+      <td>75</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie opérationnelle</td>
+      <td>70</td>
+      <td>65</td>
+      <td>40</td>
+      <td>175</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Hina</td>
+      <td>70</td>
+      <td>52</td>
+      <td>63</td>
+      <td>185</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Johny</td>
+      <td>55</td>
+      <td>53</td>
+      <td>42</td>
+      <td>150</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie pédagogique</td>
+      <td>80</td>
+      <td>55</td>
+      <td>75</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie opérationnelle</td>
+      <td>70</td>
+      <td>65</td>
+      <td>40</td>
+      <td>175</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Hina</td>
+      <td>70</td>
+      <td>52</td>
+      <td>63</td>
+      <td>185</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Johny</td>
+      <td>55</td>
+      <td>53</td>
+      <td>42</td>
+      <td>150</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie pédagogique</td>
+      <td>80</td>
+      <td>55</td>
+      <td>75</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie opérationnelle</td>
+      <td>70</td>
+      <td>65</td>
+      <td>40</td>
+      <td>175</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Hina</td>
+      <td>70</td>
+      <td>52</td>
+      <td>63</td>
+      <td>185</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Johny</td>
+      <td>55</td>
+      <td>53</td>
+      <td>42</td>
+      <td>150</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie pédagogique</td>
+      <td>80</td>
+      <td>55</td>
+      <td>75</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Ingénierie opérationnelle</td>
+      <td>70</td>
+      <td>65</td>
+      <td>40</td>
+      <td>175</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Hina</td>
+      <td>70</td>
+      <td>52</td>
+      <td>63</td>
+      <td>185</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>Johny</td>
+      <td>55</td>
+      <td>53</td>
+      <td>42</td>
+      <td>150</td>
+      <td>210</td>
+      <td>210</td>
+      <td>210</td>
+    </tr>
+  </table>
+  <Footer />
 </template>
 
 <script>
 import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
+import { onMounted } from 'vue'
 export default {
   name: 'Home',
-  components: { Header, Footer }
+  components: { Header, Footer },
+  setup () {
+    onMounted(() => {
+      const selectTd = document.querySelectorAll('td')
+      for (let i = 0; i < selectTd.length; i++) {
+        selectTd[i].classList.add('has-text-centered')
+        // Alignement centré pour chaque valeur du tableau
+      }
+      const selectTr = document.querySelectorAll('tr :nth-child(1)')
+      for (let i = 0; i < selectTr.length; i++) {
+        selectTr.forEach(() => {
+          selectTr[i].classList.add('has-text-left')
+          // Alignement à gauche pour chaque formation
+        })
+      }
+    })
+  }
 }
 </script>
